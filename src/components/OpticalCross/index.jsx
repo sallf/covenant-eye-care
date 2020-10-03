7618a407203111932cf6f645c39d98ac3ac9c447
sallf <sallf@saylorallf.com>
2020-10-02 21:24:23 -0600

    finish cross updates

--- src/components/OpticalCross/index.jsx
+++ src/components/OpticalCross/index.jsx
@@ -12,6 +12,7 @@ const OpticalCross = (props) => {
   // ---------------------
   const {
     isLink,
+    extended,
   } = props;
 
   return (
@@ -50,6 +51,57 @@ const OpticalCross = (props) => {
               )
             }
           </div>
+          {
+            extended && (
+              <>
+                <div className="col-12">
+                  <p className="text-lg text-center mt-3">Optical Cross is our new vision care savings program!</p>
+                </div>
+                <div className="col-md-6 mt-4">
+                  <p><b>Enrollment Fees</b></p>
+                  <ul>
+                    <li><p>Individual ........ $30</p></li>
+                    <li><p>Couple ............ $50</p></li>
+                    <li><p>Family ............. $70</p></li>
+                  </ul>
+                  <p>Children&#39;s exams only $99 with family enrollment.</p>
+                </div>
+                <div className="col-md-6 mt-4">
+                  <p><b>Other Benefits</b></p>
+                  <ul>
+                    <li><p>No limit on the number of times you can access savings throughout the year.</p></li>
+                    <li><p>Thirty (30) day unconditional guarantee on glasses and contact lenses.</p></li>
+                    <li><p>24 hour emergency eye care services.</p></li>
+                  </ul>
+                </div>
+                <div className="col-md-6 mt-4">
+                  <p><b>Here is a List of Your Savings...</b></p>
+                  <ul>
+                    <li><p>Save 50% on eye exams.</p></li>
+                    <li><p>Save 30% on frames.*</p></li>
+                    <li><p>Save 30% on prescription lenses.*</p></li>
+                    <li><p>Save up to 50% on a second pair of lenses.*</p></li>
+                    <li><p>Save 10-20% on contact lenses.</p></li>
+                    <li><p>Save 15% on contact lens fitting/refitting.</p></li>
+                    <li><p>Save 30% on non-prescription sunglasses.</p></li>
+                    <li><p>Save 30% on medical eye care.**</p></li>
+                  </ul>
+                  <p>Plus, savings on Corneal Reshaping (CRT) and LASIK (refractive surgery).</p>
+                  <p className="text-sm text-gray mb-0">*Limited exclusions apply.</p>
+                  <p className="text-sm text-gray">**May not be combined with any other insurance.</p>
+                </div>
+                <div className="col-md-6 mt-4">
+                  <p><b>Value Eyewear Packages</b></p>
+                  <ul>
+                    <li><p>Single Vision .................................... $149</p></li>
+                    <li><p>Bifocals ............................................ $199</p></li>
+                    <li><p>Trifocals ........................................... $219</p></li>
+                    <li><p>Progressive No-line Multifocals ...... $299</p></li>
+                  </ul>
+                </div>
+              </>
+            )
+          }
         </div>
       </div>
     </div>
@@ -58,10 +110,12 @@ const OpticalCross = (props) => {
 
 OpticalCross.defaultProps = {
   isLink: false,
+  extended: false,
 };
 
 OpticalCross.propTypes = {
   isLink: PropTypes.bool,
+  extended: PropTypes.bool,
 };
 
 export default OpticalCross;
